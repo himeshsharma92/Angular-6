f48feeb22252280228abd3dfa6a7bb934182baf7
Himesh Sharma <himeshsharma92@users.noreply.github.com>
2018-09-12 03:01:01 +0530

    Add files via upload

---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChange, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, Native can also used
+})
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, 
+AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked,
+OnDestroy, ViewChild {
+  @Input('srvElement') element:{type:string, name:string, content:string };
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+
+  constructor() {
+    console.log('constructor called!');
+   }
+
+  ngOnChanges(changes: SimpleChange){
+    console.log('ngOnChanges called!');
+    console.log(changes);
+   }
+   
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+   }
+
+  ngDoCheck(){
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called!');
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called!');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy called!');
+  }
+}
+
+
+
